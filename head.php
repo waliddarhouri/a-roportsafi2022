54e8c271a5ae2dab1afff89ede9960201f2b5bf6
[waliddarhouri] <[walidedrhouri@gmail.com]>
2020-03-28 20:30:13 +0100

    salam je suis walid j'ai modifie dans l entete

--- head.php
+++ head.php
@@ -1,6 +1,6 @@
 <meta charset="utf-8">
 <title>Aéroport SAFI</title>
-
+Salut tout le monde c est mon premier essaie
 <!-- Feuille de styles génériques -->
 <link rel="stylesheet" href="style.css" type="text/css">
 <link rel="stylesheet" href="slider.css" type="text/css">
